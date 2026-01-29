91d1371a12ff2c37cd850587e3ea58b596c67af5
ederdomingos <ederdomingos.pdbd205@pdbomdespacho.com.br>
2026-01-29 18:36:41 -0300

    Create aula3_questao2.py

---
+++ modulo6/aula3_questao2.py
@@ -0,0 +1,15 @@
+urls = [
+    "www.google.com",
+    "www.gmail.com",
+    "www.github.com",
+    "www.reddit.com",
+    "www.yahoo.com"
+]
+
+dominios = [url[4:-4] for url in urls]
+
+print("URLs:")
+print(urls)
+
+print("\nDomínios:")
+print(dominios)
