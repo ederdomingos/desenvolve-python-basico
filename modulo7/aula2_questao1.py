6198854bde86856a60e77143b14759e860544ef3
ederdomingos <ederdomingos.pdbd205@pdbomdespacho.com.br>
2026-02-09 20:09:07 -0300

    Create aula2_questao1.py

---
+++ modulo7/aula2_questao1.py
@@ -0,0 +1,12 @@
+data = input("Digite uma data de nascimento (dd/mm/aaaa): ")
+
+dia, mes, ano = data.split("/")
+
+meses = [
+    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+]
+
+nome_mes = meses[int(mes) - 1]
+
+print(f"Você nasceu em {dia} de {nome_mes} de {ano}.")
