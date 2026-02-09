9500c412251f567ce0a5fbca1b70289385081c1d
ederdomingos <ederdomingos.pdbd205@pdbomdespacho.com.br>
2026-02-09 20:09:27 -0300

    Add vowel replacement in user input phrase

---
+++ modulo7/aula2_questao2.py
@@ -0,0 +1,12 @@
+frase = input("Digite uma frase: ")
+vogais = "aeiouAEIOU"
+
+nova_frase = ""
+
+for letra in frase:
+    if letra in vogais:
+        nova_frase += "*"
+    else:
+        nova_frase += letra
+
+print("Frase modificada:", nova_frase)
